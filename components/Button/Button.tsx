9c392dde5804d4562d39fe6d9c019655b9f3b9ba
yury_sidyakin <yurafikys@gmail.com>
2024-06-29 19:21:37 +0300

    fix: дополнительно типизировал ButtonProps

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,13 +5,16 @@ import { ButtonProps } from './Button.props';
 export const Button = ({
 	appearance: appearence,
 	children,
+	className,
+	...props
 }: ButtonProps): JSX.Element => {
 	return (
 		<button
-			className={cn(styles.button, {
+			className={cn(styles.button, className, {
 				[styles.primary]: appearence == 'primary',
 				[styles.ghost]: appearence == 'ghost',
 			})}
+			{...props}
 		>
 			{children}
 		</button>
