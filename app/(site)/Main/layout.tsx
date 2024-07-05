59e2301b41834e34f898f24c9f03f7103f3e1649
yury_sidyakin <yurafikys@gmail.com>
2024-07-05 22:22:46 +0300

    feat: сделал верстку layout

--- app/(site)/Main/layout.tsx
+++ app/(site)/Main/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/Sidebar';
+import styles from './Layout.module.css';
 
 export default function MainLayout({
 	children,
@@ -8,13 +9,11 @@ export default function MainLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>{children}</div>
-			</div>
-			<Footer />
-		</>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>{children}</div>
+			<Footer className={styles.footer} />
+		</div>
 	);
 }
