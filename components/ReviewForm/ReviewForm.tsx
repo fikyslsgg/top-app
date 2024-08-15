93173dcb9cfdb65e5e18f413ae761524b1a20a7e
yury_sidyakin <yurafikys@gmail.com>
2024-08-15 20:50:44 +0300

    feat: С помощью библиотеки react-hook-form добавил функцию onSubmit на форму заполнения отзывов

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,27 +1,53 @@
 import cn from 'classnames';
+import { Controller, useForm } from 'react-hook-form';
 import { Button } from '../Button/Button';
 import { Input } from '../Input/Input';
 import { Rating } from '../Rating/Rating';
 import { Textarea } from '../Textarea/Textarea';
+import CloseIcon from './close-icon.svg';
+import { IReviewForm } from './ReviewForm.interface';
 import styles from './ReviewForm.module.css';
 import { ReviewFormProps } from './ReviewForm.props';
-import CloseIcon from './close-icon.svg';
 
 export const ReviewForm = ({
 	productId,
 	className,
 	...props
 }: ReviewFormProps): JSX.Element => {
+	const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+	const onSubmit = (data: IReviewForm) => {
+		return data;
+	};
+
 	return (
-		<>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className={cn(styles.reviewForm, className)} {...props}>
-				<Input placeholder='Имя' />
-				<Input placeholder='Заголовок отзыва' className={cn(styles.title)} />
+				<Input {...register('name')} placeholder='Имя' />
+				<Input
+					{...register('title')}
+					placeholder='Заголовок отзыва'
+					className={cn(styles.title)}
+				/>
 				<div>
 					<span>Оценка:</span>
-					<Rating className={cn(styles.rating)} rating={0} />
+					<Controller
+						control={control}
+						name='rating'
+						render={({ field }) => {
+							return (
+								<Rating
+									isEditable
+									setRating={field.onChange}
+									className={cn(styles.rating)}
+									rating={field.value}
+								/>
+							);
+						}}
+					></Controller>
 				</div>
 				<Textarea
+					{...register('description')}
 					placeholder='Текст отзыва'
 					className={cn(styles.description)}
 				/>
@@ -37,6 +63,6 @@ export const ReviewForm = ({
 				<div>спасибо, Ваш отзыв будет опубликован после проверки</div>
 				<CloseIcon className={cn(styles.close)} />
 			</div>
-		</>
+		</form>
 	);
 };
